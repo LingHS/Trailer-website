9d529c307fa086f1b580869ed210a89683ff036e
LingHS <a4643895@163.com>
2018-05-30 15:38:46 +0800

    加入ejs，bootstrap生成首页

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
+const {
+    resolve
+} = require('path')
+const ejs = require('ejs')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'ejs'
+}))
+
 app.use(async (ctx, next) => {
-    ctx.body = "movie index"
+    await ctx.render('index', {
+        you: 'Luke',
+        me: 'Scott',
+        title: '首页'
+    })
 })
 app.listen(4455)
